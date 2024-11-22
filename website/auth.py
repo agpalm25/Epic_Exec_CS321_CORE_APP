d04adf5e47ae043c2923f75b9ccea51d3944bee5
agpalm25 <agpalm25@colby.edu>
2024-11-21 20:05:57 -0500

    fixing christian's push because nothing is ever easy:D

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required
-from models import db, Admin
+from .models import Admin
+from website import db
 
 auth_blueprint = Blueprint('auth', __name__)
 
